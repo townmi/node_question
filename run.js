87a2dcc05ab5911ce38946616452b2552d8cc8f8
towne <towne766@126.com>
2016-03-16 17:10:06 +0800

    Update run.js

--- run.js
+++ run.js
@@ -12,3 +12,5 @@ console.log("打印result长度 js－－》"+result.length);
 var Method = require('./build/Release/reader');
 
 console.log("验证码识别--->"+Method(result));
+
+var utl ="http://www.fesco.com.cn/357/2015_1_13/1_357_61870_0_1421113420879.html"
